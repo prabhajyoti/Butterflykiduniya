edbe1446babd40fa9aab819f71ff6faf1f8029e2
prabhajyoti <jyotiprabha81022@gmail.com>
2024-08-10 14:14:40 +0530

    new

--- app/src/main/java/com/jyoti/butterflykiduniya/MainActivity.kt
+++ app/src/main/java/com/jyoti/butterflykiduniya/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jyoti.butterflykiduniya
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+            val explore =findViewById<Button>(R.id.explore);
+            explore.setOnClickListener{
+                val Intent =Intent(this, homepage::class.java)
+                startActivity(Intent)
+            }
         }
     }
-}
