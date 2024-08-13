5dd2d46f1ce619e59a2aad82777e00b87d9bc29c
prabhajyoti <jyotiprabha81022@gmail.com>
2024-08-13 15:53:08 +0530

    new

--- app/src/main/java/com/jyoti/butterflykiduniya/homepage.kt
+++ app/src/main/java/com/jyoti/butterflykiduniya/homepage.kt
@@ -1,6 +1,8 @@
 package com.jyoti.butterflykiduniya
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class homepage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_homepage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val detailsdisco = findViewById<Button>(R.id.detailsdisco);
+        detailsdisco.setOnClickListener{
+         val Intent = Intent(this, Activitydiscophora::class.java)
+         startActivity(Intent)
+        }
         }
     }
-}
