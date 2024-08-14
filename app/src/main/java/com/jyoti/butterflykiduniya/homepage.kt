a8095a32bf56c8b42adfc162fea3568f546bb0a8
prabhajyoti <jyotiprabha81022@gmail.com>
2024-08-14 11:32:24 +0530

    new

--- app/src/main/java/com/jyoti/butterflykiduniya/homepage.kt
+++ app/src/main/java/com/jyoti/butterflykiduniya/homepage.kt
@@ -16,9 +16,32 @@ class homepage : AppCompatActivity() {
 
 
         val detailsdisco = findViewById<Button>(R.id.detailsdisco);
-        detailsdisco.setOnClickListener{
-         val Intent = Intent(this, Activitydiscophora::class.java)
-         startActivity(Intent)
+        detailsdisco.setOnClickListener {
+            val Intent = Intent(this, Activitydiscophora::class.java)
+            startActivity(Intent)
         }
+        val detailsmormon = findViewById<Button>(R.id.detailsmormon);
+        detailsmormon.setOnClickListener {
+            val Intent = Intent(this, activitygreatmormon::class.java)
+            startActivity(Intent)
+
         }
+        val detailsjezebel = findViewById<Button>(R.id.detailsjezebel);
+        detailsjezebel.setOnClickListener {
+            val Intent = Intent(this, Activityjezebel::class.java)
+            startActivity(Intent)
+        }
+        val detailsmalbar = findViewById<Button>(R.id.detailsmalbar);
+        detailsmalbar.setOnClickListener {
+            val Intent = Intent(this, Activitymalbarrose::class.java)
+            startActivity(Intent)
+        }
+        val detailsgorgon = findViewById<Button>(R.id.detailsgorgon);
+        detailsgorgon.setOnClickListener {
+            val Intent = Intent(this, Activityyellowgorgon::class.java)
+            startActivity(Intent)
+        }
+
     }
+}
+
